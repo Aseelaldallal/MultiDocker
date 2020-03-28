26460fcd79a19a4ffd468685b33c2a65e1b86be7
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-03-27 20:02:20 -0400

    quick fix

--- worker/index.js
+++ worker/index.js
@@ -17,7 +17,7 @@ function fib(index) {
 }
 
 sub.on("message", (channel, message) => {
-  redisClient.hsetnx("values", message, fib(parseInt(message)));
+  redisClient.hset("values", message, fib(parseInt(message)));
 });
 
 sub.subscribe("insert");
